e4deda226904b62292fe93b4bfaa2c5771f5004f
Tgarrey37 <tpopp0303@gmail.com>
2022-01-14 09:26:32 -0700

    Heroku fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const path = require("path");
-const PORT = 3001;
+const PORT =process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
